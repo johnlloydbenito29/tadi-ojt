d317058b6f59b7ac252558586b5110a9cd80d207
John Lester Sabinay <sabinayjl20@gmail.com>
2025-02-17 10:39:51 +0800

    project message: Display tadi using button using #tadiModal2 in professor page

--- professor/index.php
+++ professor/index.php
@@ -90,20 +90,6 @@ echo var_dump($_SESSION);
                                     </tr>
                                 </thead>
                                 <tbody class="prof_dashboard_table">
-                                    <tr>
-                                        <td>OOP_1</td>
-                                        <td>Object Oriented Programming</td>
-                                        <td></td>
-                                        <td></td>
-                                        <td><button class="btn btn-sm w-100" style="background-color: #181a46; color: white;" data-bs-toggle="modal" data-bs-target="#tadiModal1">VIEW</button></td>
-                                    </tr>
-                                    <tr>
-                                        <td>DSC_2</td>
-                                        <td>Discrete 2</td>
-                                        <td></td>
-                                        <td></td>
-                                        <td><button class="btn btn-sm w-100" style="background-color: #181a46; color: white;" data-bs-toggle="modal" data-bs-target="#tadiModal2">VIEW</button></td>
-                                    </tr>
                                 </tbody>
                             </table>
                         </div>
@@ -138,7 +124,7 @@ echo var_dump($_SESSION);
                                 <button type="button" class="btn w-100" style="background-color: #181a46; color: white;">Search</button>
                                 </div>
                             </div>
-             <!-- SUBJECT MODAL -->
+             <!-- TADI_SUBJECT MODAL -->
             
                             <div class="mt-4">
                 <div class="card shadow-sm">
@@ -161,91 +147,73 @@ echo var_dump($_SESSION);
                     </div>
                 </div>
             </div>
-                        
-                            <!-- <div class="accordion accordion-flush mt-3" id="accordionFlushExample">
-                                <div class="accordion-item">
-                                <h2 class="accordion-header">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                                <div class="d-flex justify-content-between w-100">
-                                <div id = "student_name">Louise Mae Lopez</div>
-                                    <div class="text-end">
-                                    <span class="badge me-2" style="color: black;">Approved by: Jane Doe</span>
-                                    <span class="badge me-2" style="color: black;">Feb 2 2025</span>
 
-                                
-                                </div>
-                                </div>
-                                </button>
-                                </h2>
-                                <!-- <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                                    <div class="accordion-body">
-                                    <div class="row">
-                                        <div class="col-md-4">
-                                        <h6>Professor Name:</h6>
-                                        <p>John Artemson De Guzman</p>
-                                        </div>
-                                        <div class="col-md-4">
-                                        <h6>Mode of Class:</h6>
-                                        <p>Asynchronous</p>
-                                        </div>
-                                        <div class="col-md-4">
-                                        <h6>Type of Class:</h6>
-                                        <p>Regular Class</p>
-                                        </div>
-                                    </div>
-                                    <div class="row mt-4">
-                                        <div class="col-md-4">
-                                        <h6>Class Start Schedule:</h6>
-                                        <p>October 26, 2023 1:00 PM</p>
-                                        </div>
-                                        <div class="col-md-4">
-                                        <h6>Class End Schedule:</h6>
-                                        <p>October 26, 2023 4:00 PM</p>
-                                        </div>
-                                    </div>
-                                    <div class="row mt-4">
-                                        <div class="col-12">
-                                        <h6>Report:</h6>
-                                        <div class="border border-secondary-subtle p-3 border-1 rounded">
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                                        </div>
-                                        </div>
-                                    </div>
-                                    </div>
-                                </div>
-                                </div>
-                                
-                                    <div class="accordion-item">
-                                <h2 class="accordion-header">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                                    Accordion Item #3 (Example)
-                                    </button>
-                                </h2>
-                                <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                                    <div class="accordion-body">
-                                    <p>Content for Accordion Item 3.  You can add more information here.</p>
-                                    <div class="row mt-4">
-                                        <div class="col-md-4">
-                                        <h6>Class Start Schedule:</h6>
-                                        <p>November 9, 2023 10:00 AM</p>
-                                        </div>
-                                        <div class="col-md-4">
-                                        <h6>Class End Schedule:</h6>
-                                        <p>November 9, 2023 1:00 PM</p>
-                                        </div>
-                                    </div>
-                                    </div>
-                                </div>
-                                </div>
-                            </div> -->
+                 
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                             
                             </div>
                             </div>
-                        </div> -->
+                        </div> 
                     </div>
                 </div>
+
+                <div class="modal fade" id="tadiModal2" tabindex="-1" aria-labelledby="tadiModalLabel" aria-hidden="true" data-bs-backdrop="static">
+                <div class="modal-dialog modal-dialog-centered modal-xl">
+                    <div class="modal-content">
+                        <div class="modal-header d-flex justify-content-between align-items-start" style="background-color: #181a46; color: white;">
+                            <div class="subject-info">
+                                <h5 class="modal-title" id="tadi_modal_label"></h5>
+                                <p id="subject_code" class="subject-details mb-0"></p>
+                                <p id="tadi_date" class="mb-0"></p>
+                            </div>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row mt-4">
+                                <div class="col flex">
+                                    <h6>Professor Name:</h6>
+                                    <p id="prof_name"></p>
+                                </div>
+
+                                <div class="col flex">
+                                    <h6>Learning Delivery Modalities:</h6>
+                                    <p id="modalities"></p>
+                                </div>
+
+                                <div class="col flex">
+                                    <h6>Session Type:</h6>
+                                    <p id="session_type"></p>
+                                </div>
+
+                                <div class="row my-4">
+                                    <div class="col-4 flex">
+                                        <h6>Class Start Time:</h6>
+                                        <p id="time_in"></p>
+                                    </div>
+
+                                    <div class="col flex">
+                                        <h6>Class End Time:</h6>
+                                        <p id="time_out"></p>
+                                    </div>
+                                    <div class="col-12 mt-4">
+                                        <div class="flex">
+                                            <h6 class="mb-4">Remarks:</h6>
+
+                                            <div class="border border-secondary-subtle p-3 border-1 rounded">
+                                                <p id="report"> </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn" style="background-color: #181a46; color: white;" data-bs-dismiss="modal">Not Approve</button>
+                                        <button type="button" class="btn" style="background-color: #181a46; color: white;" data-bs-dismiss="modal">Approve</button>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
             </div>
         </div>
     </section>
--- professor/view/index-function.js
+++ professor/view/index-function.js
@@ -181,32 +181,22 @@ function DISPLAY_PROFESSOR_SUBJECT(result) {
     
 
     $(document).on("click", tadiHandler, function () {
-      displayModal(value, index);
+      displayModalHeader(value, index);
       GET_TADI_SUBJ_LIST(value.subj_id) // get tadi list
       
     });
   });
 }
 
-
-function displayModal(value) {
-
-  // console.log("value =>", value);
-  
-  let formattedDate = new Date(value.tadi_date).toLocaleDateString("en-PH", {
-    month: "long",
-    day: "numeric",
-    year: "numeric",
-  });
-  
-
+    // prof_tadi_summary_modal
+function displayModalHeader(value) {  
+    
   $("#tadi_subj_name").text(value.subj_desc);
   $("#section_name").text(value.subj_sec_name);
 }    
 
 
 function displaySectionTableModal(result) {
- console.log('section =>', result);
 
   var count = 1;
   const tableRows = result.length
@@ -215,12 +205,12 @@ function displaySectionTableModal(result) {
         $.each([value], function (key, item) {
           acc += `
 
-                  <tr key="${item.subj_id}">
+                  <tr key="${item.schltadi_id}">
                       <td>${count}</td>
                       <td>${item.stud_name}</td>
                       <td>${new Date(item.schltadi_date).toLocaleDateString('en-US', {month: 'long', day: 'numeric', year: 'numeric'})}</td>
                       <td><button class="btn btn-sm w-100" 
-                      style="background-color: #181a46; color: white;" id="tadiModalHandler${index}" data-bs-toggle="modal" data-bs-target="#tadiModal">VIEW</button></td>
+                      style="background-color: #181a46; color: white;" id="tadiModalHandler${index}" data-bs-toggle="modal" data-bs-target="#tadiModal2">VIEW</button></td>
                   </tr>
                 `;
 
@@ -255,47 +245,34 @@ function GET_TADI_SUBJ_LIST(value) {
     success: function (result) {
       console.log("tadi =>",result);
       displaySectionTableModal(result)
+      
 
     },
   });
 }
 
-// function display_Section_List(result) {
-//   // console.log('section =>', result);
 
-//   var count = 1;
-//   const tableRows = result.length
-//     ? result
-//       .reduce((acc, value, index) => {
-//         $.each([value], function (key, item) {
-//           acc += `
 
-//                   <tr key="${item.subj_code}">
-//                       <td>${count}</td>
-//                       <td>${item.stud_name}</td>
-//                       <td>${new Date(item.schltadi_date).toLocaleDateString('en-US', {month: 'long', day: 'numeric', year: 'numeric'})}</td>
-//                       <td><button class="btn btn-sm w-100" ${item.prof_name ? item.prof_name : "disabled"
-//             } style="background-color: #181a46; color: white;" id="tadiModalHandler${index}" data-bs-toggle="modal" data-bs-target="#tadiModal">VIEW</button></td>
-//                   </tr>
-//                 `;
+function displayModal(value) {
 
-//                 count++;
-//         });
-//         return acc;     
-//       }, "")
-//     : `<tr>
-//           <td colspan="5" class="text-center">No tadi forms available</td>
-//       </tr>`;
+  console.log("modal",value);
+  let formattedDate = new Date(value.tadi_date).toLocaleDateString("en-PH", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+  
 
-//   $(".prof_section_table").html(tableRows);
-
-//   result.filter((value, index) => {
-//     const tadiHandler = `#tadiModalHandler${index}`;  
-//     $(document).on('click', tadiHandler, function () {
-//       displayModal(value, index);
-//     });
-//   });
-// }
-
+  $("#tadi_modal_label").text(value.subj_name);
+  $("#subject_code").text(value.subj_code);
+  $("#tadi_date").text(formattedDate);
+  $("#prof_name").text(value.prof_name);
+  $("#prof_name").text(value.prof_name);
+  // $("#modalities").text(value.tadi_mode.toUpperCase().replace(/_/g, ' '));
+  // $("#session_type").text(value.tadi_type.toUpperCase().replace(/_/g, ' '));
+  $("#time_in").text(convert12HourFormat(value.time_in));
+  $("#time_out").text(convert12HourFormat(value.time_out));
+  $("#report").text(value.tadi_activity);
+}
 
 
